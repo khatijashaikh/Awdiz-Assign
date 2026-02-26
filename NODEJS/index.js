f6c91bf1dfd22bb39bb9b1332e50b945b3418cdf
khatija <shaikhkhatijabi03@gmail.com>
2026-02-26 13:35:49 +0530

    mongoose

--- NODEJS/index.js
+++ NODEJS/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import AllRoutes from "./routes/index.js";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
+dotenv.config();
 
 app.get('/', (req, res)=>{
     res.send("welome to backend server")
@@ -15,7 +17,7 @@ app.get('/', (req, res)=>{
 
 app.use("/api/v1", AllRoutes);
 
-// mongoose.connect()
+mongoose.connect(process.env.MONGODBURL).then(()=>{console.log("Mongo connected")});
 
 
 
--- NODEJS/models/user.schema.js
+++ NODEJS/models/user.schema.js
@@ -1,13 +1,14 @@
-// import { model, Schema} from 'mongoose';
+import { model, Schema} from 'mongoose';
 
-// const userSchema = new Schema({
-//     name: { type: String, required: true},
-//     email:{ type: String, required: true},
-//     password: { type: String, required: true},
-//     phone: { type: Number, default: 97765578},
-// });
+const userSchema = new Schema({
+    name: { type: String, required: true},
+    email:{ type: String, required: true},
+    password: { type: String, required: true},
+    phone: { type: Number, default: 97765578},
+});
+
+const User = model("User", userSchema)
 
 
 
-
-// export default User;
+export default User;
